c731609e0da0cd0e07e87965249f6279fe5c72be
antbird <mcintyre.michael.t@gmail.com>
2013-11-23 16:17:22 -0500

    Add newGame method to app

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,12 @@
 var GAME = {};
 GAME.data = {};
+
+function newGame() 
+{
+    console.log("Game started!");
+    GAME.player = new GAME.Player();
+    GAME.world = new GAME.World();
+    GAME.WorldController.newLevel();
+}
+
+window.onload = newGame;
