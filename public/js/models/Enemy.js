09f3d4246ce3b3e9046a3b814911ee9435bbdf92
antbird <mcintyre.michael.t@gmail.com>
2013-11-23 15:55:42 -0500

    Refactor Enemy model

--- public/js/models/Enemy.js
+++ public/js/models/Enemy.js
@@ -1,9 +1,9 @@
 GAME.Enemy = function(enemy)
 {   
-    this.id = ++GAME.World.enemy_counter;
+    this.id = ++GAME.world.counters.enemies;
     this.enemy_id = enemy.id;
     this.name = enemy.name;
-    this.level = Math.floor(Math.random()*((GAME.World.level*1.2)-(GAME.World.level*0.85)+1)+(GAME.World.level*0.85));
+    this.level = Math.floor(Math.random()*((GAME.world.counters.level*1.2)-(GAME.world.counters.level*0.85)+1)+(GAME.world.counters.level*0.85));
     
     this.attack = Math.ceil(this.level * enemy.attack);
     this.defense = Math.ceil(this.level * enemy.defense);
