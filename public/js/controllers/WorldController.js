960e3b78afadc8457519b1c22abfa452984e52a6
antbird <mcintyre.michael.t@gmail.com>
2013-11-23 15:54:53 -0500

    Add simple world generation to World controller

--- public/js/controllers/WorldController.js
+++ public/js/controllers/WorldController.js
@@ -1,7 +1,28 @@
 GAME.WorldController =
 {
-    newLevel: function() {
+    newLevel: function() 
+    {
         ++GAME.world.counters.level;        
+        this.generateTiles();
+        this.renderWorld();
+    },
+    
+    generateTiles: function() 
+    {
+        GAME.world.tiles = [];
+        for (var h = 0; h < GAME.world.height; h++)
+        {
+            GAME.world.tiles[h] = [];
+            for (var w = 0; w < GAME.world.width; w++)
+            {
+                GAME.world.tiles[h][w] = GAME.TileController.newTile();
+            }
+        }        
+    },
+
+    renderWorld: function() 
+    {
+        GAME.WorldView.render();
     }
 
 };
