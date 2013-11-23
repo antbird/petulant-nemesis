f2966a2b801d996ee5568f3480daba0a3253d7d2
antbird <mcintyre.michael.t@gmail.com>
2013-11-23 15:58:32 -0500

    Create initial tile data

---
+++ public/js/data/tiles.js
@@ -0,0 +1,18 @@
+GAME.data.tiles =
+{
+    1: {
+        id: 1,
+        name: "Normal",
+        class_name: "normal"
+    },
+    2: {
+        id: 2,
+        name: "Wall",
+        class_name: "wall"
+    },
+    3: {
+        id: 3,
+        name: "Water",
+        class_name: "water"
+    }
+};
