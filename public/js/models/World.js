da92d75386919935a350e8ae8ccfaac89a509f4b
antbird <mcintyre.michael.t@gmail.com>
2013-11-23 12:35:10 -0500

    Create World model

---
+++ public/js/models/World.js
@@ -0,0 +1,12 @@
+GAME.World =
+{
+    level: 1,
+    tile_counter: 0,
+    item_counter: 0,
+    enemy_counter: 0,
+    treasure_counter: 0,
+    tiles: [],
+    items: [],
+    enemies: [],
+    treasure: []
+};
