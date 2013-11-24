133e09c8f0a11c49ba623319ec271902a141f1a2
antbird <mcintyre.michael.t@gmail.com>
2013-11-24 13:31:45 -0500

    Add shuffle function to Array prototype, implementing Fisher-Yates algorithm

--- public/js/app.js
+++ public/js/app.js
@@ -10,3 +10,15 @@ GAME.newGame = function()
 };
 
 window.onload = GAME.newGame;
+
+//implement Fisher-Yates algorithm for array shuffling
+Array.prototype.shuffle = function (){
+    var i = this.length, j, temp;
+    if ( i == 0 ) return;
+    while ( --i ) {
+        j = Math.floor( Math.random() * ( i + 1 ) );
+        temp = this[i];
+        this[i] = this[j];
+        this[j] = temp;
+    }
+};
