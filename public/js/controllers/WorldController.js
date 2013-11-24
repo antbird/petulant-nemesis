fc50dd873db560d681979675117da8136a96b71e
antbird <mcintyre.michael.t@gmail.com>
2013-11-23 20:52:10 -0500

    Update World controller, add room and hallway generation

--- public/js/controllers/WorldController.js
+++ public/js/controllers/WorldController.js
@@ -1,27 +1,87 @@
 GAME.WorldController =
 {
-    newLevel: function() 
-    {
-        ++GAME.world.counters.level;        
-        this.generateTiles();
+    newLevel: function() {
+        ++GAME.world.counters.level;
+        this.generateMap();
+        this.generateRooms(GAME.world.roomcount);  
+        this.generateHallways(GAME.world.roomcount);
         this.renderWorld();
     },
     
-    generateTiles: function() 
-    {
-        GAME.world.tiles = [];
-        for (var h = 0; h < GAME.world.height; h++)
+    generateMap: function() {
+        GAME.world.level.tiles = [];
+        GAME.world.counters.tiles = 0;
+        for (var y = 0; y < GAME.world.height; y++)
         {
-            GAME.world.tiles[h] = [];
-            for (var w = 0; w < GAME.world.width; w++)
+            GAME.world.level.tiles[y] = [];
+            for (var x = 0; x < GAME.world.width; x++)
             {
-                GAME.world.tiles[h][w] = GAME.TileController.newTile();
+                GAME.world.level.tiles[y][x] = GAME.TileController.newTile(2); //fills all spaces with a wall tile, update later
             }
         }        
     },
+    
+    generateRooms: function(numRooms) {
+        GAME.world.level.rooms = [];
+        for (var i =0; i < numRooms; i++) {
+            GAME.world.level.rooms[i] = GAME.RoomController.newRoom();
+            for(var y = GAME.world.level.rooms[i].y; y < GAME.world.level.rooms[i].y + GAME.world.level.rooms[i].h; y++) {
+                for(var x = GAME.world.level.rooms[i].x; x < GAME.world.level.rooms[i].x + GAME.world.level.rooms[i].w; x++) {
+                    GAME.world.level.tiles[y][x] = this.generateTile();                
+                }                
+            } 
+        }        
+    },
+    
+    generateHallways: function(numRooms) {       
+        for (var i = 0; i < numRooms; i++) {
+            var roomA = GAME.world.level.rooms[i],
+                pointA = {
+                    x: Math.floor(Math.random()*roomA.w) + roomA.x,
+                    y: Math.floor(Math.random()*roomA.h) + roomA.y
+                },
+                roomNum = i;
 
-    renderWorld: function() 
-    {
+            while(roomNum == i){
+                roomNum = Math.floor(Math.random()*GAME.world.level.rooms.length);
+            }
+
+            var roomB = GAME.world.level.rooms[roomNum],                
+                pointB = {
+                    x: Math.floor(Math.random()*roomB.w) + roomB.x,
+                    y: Math.floor(Math.random()*roomB.h) + roomB.y
+                };
+                
+            while (pointB.x !== pointA.x || pointB.y !== pointA.y){
+                if (Math.random()*100 < 25){
+                    if (pointB.x !== pointA.x){
+                        if(pointB.x > pointA.x){
+                            pointB.x--;
+                        } else {
+                            pointB.x++;
+                        }
+                    }
+                } else if(pointB.y !== pointA.y){
+                    if(pointB.y > pointA.y){
+                        pointB.y--;
+                    } else {
+                        pointB.y++;
+                    }
+                }
+
+                if(pointB.x < GAME.world.width && pointB.y < GAME.world.width){         
+                    GAME.world.level.tiles[pointB.y][pointB.x] = this.generateTile();
+                }
+            }
+        }        
+    },    
+    
+    generateTile: function() {
+        //add some logic here to determine which tiles to use
+        return GAME.TileController.newTile(1);
+    },
+
+    renderWorld: function() {
         GAME.WorldView.render();
     }
 
