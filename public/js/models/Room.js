f3170ddaadcd28cdb7ca714dd49ba95c9b5307c6
antbird <mcintyre.michael.t@gmail.com>
2013-11-23 20:50:00 -0500

    Create Room model

---
+++ public/js/models/Room.js
@@ -0,0 +1,9 @@
+GAME.Room = function(x,y,w,h,center_x,center_y)
+{    
+    this.x = x;
+    this.y = y;
+    this.h = h;
+    this.w = w;
+    this.center_x = center_x;
+    this.center_y = center_y;
+};
