1437b051895651d58ca9a857acc84c6c50b5129a
antbird <mcintyre.michael.t@gmail.com>
2013-11-24 13:30:37 -0500

    Add solid property to tiles

--- public/js/data/tiles.js
+++ public/js/data/tiles.js
@@ -3,16 +3,19 @@ GAME.data.tiles =
     1: {
         id: 1,
         name: "Normal",
-        class_name: "normal"
+        class_name: "normal",
+        solid: 0
     },
     2: {
         id: 2,
         name: "Wall",
-        class_name: "wall"
+        class_name: "wall",
+        solid: 1
     },
     3: {
         id: 3,
         name: "Water",
-        class_name: "water"
+        class_name: "water",
+        solid: 1
     }
 };
