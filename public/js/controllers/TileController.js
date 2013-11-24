8b990f0492bc1b1a759aa67cec4db54a9c4c4032
antbird <mcintyre.michael.t@gmail.com>
2013-11-23 20:50:44 -0500

    Refactor newTile method

--- public/js/controllers/TileController.js
+++ public/js/controllers/TileController.js
@@ -1,6 +1,9 @@
 GAME.TileController =  
 {
-    newTile: function(){
+    newTile: function(type){
+        if (type !== 0) {
+            return new GAME.Tile(GAME.data.tiles[type]);
+        }
         var tile_id = Math.floor(Math.random() * 3) + 1;
         if (tile_id === 3) {
             tile_id = Math.floor(Math.random() * 3) + 1;
