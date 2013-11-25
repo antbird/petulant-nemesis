36f9d81f40c921f231d445a347413070cbfca06c
antbird <mcintyre.michael.t@gmail.com>
2013-11-24 21:59:48 -0500

    Add render player method to World view. Refactor tile id/classname retrieval

--- public/js/views/WorldView.js
+++ public/js/views/WorldView.js
@@ -1,15 +1,27 @@
 GAME.WorldView = 
 {
-    render: function() {
+    renderTiles: function() {
         var container = document.getElementById("world");        
         var tiles = '';
         GAME.world.level.tiles.forEach(function (column) 
         {
             column.forEach(function (tile) {                
-                tiles += "<span id=\"tile_" + tile.id + "\" class=\"tile tile-" + GAME.world.tile_size + " " + tile.class_name + "\"></span>\n";                
+                tiles += "<span id=\"tile_" + tile.id + "\" class=\"" + GAME.data.tiles[tile.tile_id].class_name + "\"></span>\n";                
             });
             tiles += "<br/>";            
         });
         container.innerHTML = tiles;
+    },
+    
+    renderPlayer: function() {
+        this.unrenderPlayer();
+        var tile = GAME.world.level.tiles[GAME.world.level.player.y][GAME.world.level.player.x],
+            container = document.getElementById("tile_" + tile.id);        
+        container.innerHTML = "<img id=\"player\" src=\"http://thechetfaliszeks.com/petulance/img/player.png\">";
+    },
+    
+    unrenderPlayer: function() {
+        if (document.getElementById("player"))
+            document.getElementById("player").remove();
     }
 };
