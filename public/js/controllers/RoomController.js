522d34fd254b81110095ca90380f64379293ac70
antbird <mcintyre.michael.t@gmail.com>
2013-11-24 22:04:08 -0500

    Nothing of importance

--- public/js/controllers/RoomController.js
+++ public/js/controllers/RoomController.js
@@ -8,8 +8,7 @@ GAME.RoomController =
                 w = Math.floor(Math.random() * GAME.world.maxRoomSize) + GAME.world.minRoomSize,
                 h = Math.floor(Math.random() * GAME.world.maxRoomSize) + GAME.world.minRoomSize,
                 center_x = x + (w / 2),
-                center_y = y + (h / 2);
-            
+                center_y = y + (h / 2);            
             
             if (x + w >= GAME.world.width || y + h >= GAME.world.height) {
                 continue;
@@ -18,5 +17,4 @@ GAME.RoomController =
             return new GAME.Room(x,y,w,h,center_x,center_y);
         }
     }
-
 };
