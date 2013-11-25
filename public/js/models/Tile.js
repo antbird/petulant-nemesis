ccc97a0f6023a5aabe6aea599db3dfde451931c3
antbird <mcintyre.michael.t@gmail.com>
2013-11-24 20:38:32 -0500

    Remove Tile model

--- public/js/models/Tile.js
+++
@@ -1,8 +0,0 @@
-GAME.Tile = function(tile) 
-{
-    this.id = ++GAME.world.counters.tiles;
-    this.tile_id = tile.id;
-    this.name = tile.name;
-    this.class_name = tile.class_name;
-    this.solid = tile.solid;
-};
