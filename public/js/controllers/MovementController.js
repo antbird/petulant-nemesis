2613a432e2510711c3c9ddc85ae9fa9305fb5047
antbird <mcintyre.michael.t@gmail.com>
2013-11-24 22:02:22 -0500

    Refactor move method and checkSolid method

--- public/js/controllers/MovementController.js
+++ public/js/controllers/MovementController.js
@@ -1,22 +1,18 @@
 GAME.MovementController =
 {
     move: function(dir,xy_obj) {
-        if (dir === 'W') {
-            if (!this.checkSolid(xy_obj.x-1,xy_obj.y))
-                --xy_obj.x;
-        } else if (dir === 'E') {
-            if (!this.checkSolid(xy_obj.x+1,xy_obj.y))
-                ++xy_obj.x;
-        } else if (dir === 'N') {
-            if (!this.checkSolid(xy_obj.x,xy_obj.y-1))
-                --xy_obj.y;
-        } else if (dir === 'S') {
-            if (!this.checkSolid(xy_obj.x,xy_obj.y+1))
-                ++xy_obj.y;
+        if (dir === 'W' && !this.checkSolid(xy_obj.x-1,xy_obj.y)) {
+            --xy_obj.x;
+        } else if (dir === 'E' && !this.checkSolid(xy_obj.x+1,xy_obj.y)){
+            ++xy_obj.x;
+        } else if (dir === 'N' && !this.checkSolid(xy_obj.x,xy_obj.y-1)){
+            --xy_obj.y;
+        } else if (dir === 'S' && !this.checkSolid(xy_obj.x,xy_obj.y+1)){
+            ++xy_obj.y;
         }
     },
     
     checkSolid: function(x,y) {
-        return GAME.world.level.tiles[y][x].solid;             
+        return GAME.data.tiles[GAME.world.level.tiles[y][x].tile_id].solid;             
     }
 };
